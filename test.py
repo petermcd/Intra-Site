c41e84580eb19409aca74284dbf40eb6dd100ce3
Peter McDonald <me@petermcdonald>
2021-08-23 19:48:23 +0100

    Added book download field

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from json import loads, dumps
 API_KEY = 'AIzaSyAemadkCuLLWhPAI7jBgLQIOC56yGjSNXU'
 URL = 'https://www.googleapis.com/books/v1/volumes?'
 search_type = 'isbn'
-search = '1635576059'
+search = '1491936160'
 querystring = f'q={search_type}:{search}&key={API_KEY}'
 res = requests.get(f'{URL}{querystring}')
 l = loads(res.content)
