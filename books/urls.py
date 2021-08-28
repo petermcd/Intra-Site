72f8a957680413628aa99bcfff6105845e691d17
Peter McDonald <me@petermcdonald>
2021-08-28 15:49:43 +0100

    Corrected names of urls added events to the header

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,5 @@ from books import views
 app_name = 'books'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('<int:pk>/', views.DetailView.as_view(), name="index"),
+    path('<int:pk>/', views.DetailView.as_view(), name="details"),
 ]
--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,5 @@ from events import views
 app_name = 'events'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('<int:pk>/', views.DetailView.as_view(), name="index"),
+    path('<int:pk>/', views.DetailView.as_view(), name="details"),
 ]
