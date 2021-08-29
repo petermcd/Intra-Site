e3aacc019aa3bc5d1625976b566fa01c02ddecb3
Peter McDonald <me@petermcdonald>
2021-08-29 22:05:43 +0100

    Corrected sorting for events

--- events/views.py
+++ events/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
     context_object_name = 'event_list'
 
     def get_queryset(self):
-        return Event.objects.all().filter(ending__gte=now())
+        return Event.objects.all().filter(ending__gte=now()).order_by('starting')
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
