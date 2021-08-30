c77726160d45baee940eef7487593fb2364b541d
Peter McDonald <148006+petermcd@users.noreply.github.com>
2021-08-31 00:19:25 +0100

    Implemented book search and pagination (#56)

--- books/views.py
+++ books/views.py
@@ -4,11 +4,16 @@ from books.models import Book
 
 
 class IndexView(generic.ListView):
+    paginate_by = 10
     template_name = 'books/index.html'
     context_object_name = 'book_list'
 
     def get_queryset(self):
-        return Book.objects.all()
+        term = self.request.GET.get('q')
+        objects = Book.objects.all().order_by('title')
+        if term:
+            objects = Book.objects.filter(title__icontains=term).order_by('title')
+        return objects
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs)
