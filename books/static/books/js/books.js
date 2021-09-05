bd5e4d4e1bd506ca37f508f436fcc3d6a374603f
Peter McDonald <148006+petermcd@users.noreply.github.com>
2021-09-05 16:11:51 +0100

    Template refactor and js lint fixes (#60)

--- books/static/books/js/books.js
+++ books/static/books/js/books.js
@@ -1,3 +1,5 @@
+// jshint esversion: 6
+
 $(document).ready(function() {
         $('#id_isbn10').after('<button id="isbn10_lookup">Fetch</button>');
         $('#isbn10_lookup').click(function(event){
