b86194bc5014ef1bf278ebd88cef40da7e6623c3
Peter McDonald <148006+petermcd@users.noreply.github.com>
2021-09-07 22:08:19 +0100

    Date format change (#64)

--- manage.py
+++ manage.py
@@ -15,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    sys.argv = ['D:/Python_Projects/Intra-Site/manage.py', 'runserver', '8000']
     execute_from_command_line(sys.argv)
 
 
