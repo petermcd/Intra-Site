d86d66606b94e2c1e15e1023962eddc97f241240
Peter McDonald <148006+petermcd@users.noreply.github.com>
2021-09-08 19:20:34 +0100

    refactored books.js and resolved unsanitised input (#65)

--- books/static/books/js/books.js
+++ books/static/books/js/books.js
@@ -1,61 +1,69 @@
 // jshint esversion: 6
 
-$(document).ready(function() {
-        $('#id_isbn10').after('<button id="isbn10_lookup">Fetch</button>');
-        $('#isbn10_lookup').click(function(event){
-                event.preventDefault();
-                isbn10_lookup();
-        });
+$(document).ready(function () {
+    $('#id_isbn10').after('<button id="isbn10_lookup">Fetch</button>');
+    $('#isbn10_lookup').click(function (event) {
+        event.preventDefault();
+        isbn10_lookup();
+    });
 });
 
-function isbn10_lookup(){
-        let url = '/api/book/isbn/' + $('#id_isbn10').val();
-        remove_lookup_message();
-        $.ajax({
-                url: url,
-                context: document.body
-        }).done(
-            function(data){
-                    if (data['success'] === true && data['records'] > 0) {
-                            populate_form(data['data'][0]);
-                    }
-                    else {
-                            report_lookup_failure('No book found');
-                    }
+function isbn10_lookup() {
+    let url = '/api/book/isbn/' + $('#id_isbn10').val();
+    remove_lookup_message();
+    $.ajax({
+        url: url,
+        context: document.body
+    }).done(
+        function (data) {
+            if (data['success'] === true && data['records'] > 0) {
+                populate_form(data['data'][0]);
+            } else {
+                report_lookup_failure('No book found');
             }
-        ).fail(
-            function(){
-                    report_lookup_failure('No book found');
-            });
-}
-
-function populate_form(form_data){
-        $('#id_title').val(form_data['title']);
-        $('#id_subtitle').val(form_data['subtitle']);
-        $('#id_publisher').val(form_data['publisher']);
-        $('#id_published').val(form_data['published']);
-        $('#id_description').val(form_data['description']);
-        $('#id_pages').val(form_data['pages']);
-        $('#id_thumbnail').val(form_data['thumbnail']);
-        $('#id_isbn10').val(form_data['isbn10']);
-        $('#id_isbn13').val(form_data['isbn13']);
-        let $author_select = $('#id_authors');
-        for (let author of form_data['authors']){
-                if (!option_exists(author['id'])){
-                        $author_select.append(new Option(author['name'], author['id']));
-                }
-                $author_select.val(author['id']).change();
         }
+    ).fail(
+        function () {
+            report_lookup_failure('No book found');
+        }
+    );
 }
 
-function option_exists($value){
-        return $('#id_authors option[value=' + $value + ']').length > 0;
+function populate_form(form_data) {
+    $('#id_title').val(form_data['title']);
+    $('#id_subtitle').val(form_data['subtitle']);
+    $('#id_publisher').val(form_data['publisher']);
+    $('#id_published').val(form_data['published']);
+    $('#id_description').val(form_data['description']);
+    $('#id_pages').val(form_data['pages']);
+    $('#id_thumbnail').val(form_data['thumbnail']);
+    $('#id_isbn10').val(form_data['isbn10']);
+    $('#id_isbn13').val(form_data['isbn13']);
+    let $author_select = $('#id_authors');
+    for (let author of form_data['authors']) {
+        if (!option_exists(author['id'])) {
+            $author_select.append(new Option(author['name'].htmlEscape(), author['id'].htmlEscape()));
+        }
+        $author_select.val(author['id']).change();
+    }
 }
 
-function report_lookup_failure($message){
-        $('#isbn10_lookup').after('<p style="color: red" id="lookup_failure">' + $message + '</p>');
+function option_exists($value) {
+    return $('#id_authors option[value=' + $value + ']').length > 0;
 }
 
-function remove_lookup_message(){
-        $('#lookup_failure').remove();
+function report_lookup_failure($message) {
+    $('#isbn10_lookup').after('<p style="color: red" id="lookup_failure">' + $message + '</p>');
 }
+
+function remove_lookup_message() {
+    $('#lookup_failure').remove();
+}
+
+String.prototype.htmlEscape = function () {
+    var span = document.createElement('span');
+    var txt = document.createTextNode('');
+    span.appendChild(txt);
+    txt.data = this;
+    return span.innerHTML;
+};
