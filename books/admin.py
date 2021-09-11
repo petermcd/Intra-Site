640740c178cdadbffbdb99b7d77ce56083baa801
Peter McDonald <148006+petermcd@users.noreply.github.com>
2021-09-11 14:48:49 +0100

    Tidying code (#68)

--- books/admin.py
+++ books/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from books.models import Author, Book
 
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+    ordering = ['name', ]
+
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'subtitle', 'publisher', 'isbn10', 'read')
     ordering = ('title',)
@@ -12,7 +19,3 @@ class BookAdmin(admin.ModelAdmin):
             '//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js',
             'books/js/books.js',
         )
-
-
-admin.site.register(Author)
-admin.site.register(Book, BookAdmin)
--- finance/admin.py
+++ finance/admin.py
@@ -3,27 +3,25 @@ from django.contrib import admin
 from .models import Bill, Company, Debt, Investment
 
 
+@admin.register(Bill)
 class BillAdmin(admin.ModelAdmin):
     list_display = ('company', 'payment_amount_clean', 'payment_day',)
     ordering = ('company',)
 
 
+@admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'url',)
     ordering = ('name',)
 
 
+@admin.register(Debt)
 class DebtAdmin(admin.ModelAdmin):
     list_display = ('name', 'payment_amount_clean', 'current_balance_clean', 'remaining_payment_count',)
     ordering = ('company',)
 
 
+@admin.register(Investment)
 class InvestmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'company', 'value_clean',)
     ordering = ('name', 'company',)
-
-
-admin.site.register(Bill, BillAdmin)
-admin.site.register(Company, CompanyAdmin)
-admin.site.register(Debt, DebtAdmin)
-admin.site.register(Investment, InvestmentAdmin)
