46d35af3f20e0b1725e32d112cd35a26506d08a4
Peter McDonald <me@petermcdonald>
2022-04-10 14:58:05 +0100

    Fixed favicon.ico

--- intranet/urls.py
+++ intranet/urls.py
@@ -1,6 +1,8 @@
 """Primary URL configuration for the Intranet site."""
 from django.contrib import admin
+from django.templatetags.static import static
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 from intranet.views import IndexView
 
@@ -10,6 +12,7 @@ urlpatterns = [
     path("books/", include("books.urls")),
     path("documents/", include("documents.urls")),
     path("downloads/", include("downloads.urls")),
+    path("favicon.ico", RedirectView.as_view(url=static("img/favicon.ico"))),
     path("finance/", include("finance.urls")),
     path("tasks/", include("tasks.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
