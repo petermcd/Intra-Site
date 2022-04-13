ce3d856cf1a020fccf6da588ab0a07e863d34252
Peter McDonald <148006+petermcd@users.noreply.github.com>
2022-04-13 20:01:23 +0100

    Network (#139)
    * moved downloads to documents

--- documents/models.py
+++ documents/models.py
@@ -7,5 +7,5 @@ class Document(models.Model):
 
     title: models.CharField = models.CharField(max_length=200)
     description: models.CharField = models.CharField(max_length=255, blank=True)
-    document: models.FileField = models.FileField(upload_to="downloads/documents/")
+    document: models.FileField = models.FileField(upload_to="documents/documents/")
     uploaded_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
