873763641db833604dca2cc57d8ddc74d86841c2
Peter McDonald <me@petermcdonald>
2022-04-13 22:40:44 +0100

    Corrected STATICFILES_DIRS

--- intranet/settings.py
+++ intranet/settings.py
@@ -144,6 +144,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_FILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
