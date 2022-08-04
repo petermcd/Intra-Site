52bced3099234b62769f37aba5f90d7939bc7cc7
Peter McDonald <148006+petermcd@users.noreply.github.com>
2022-08-05 00:24:22 +0100

    Task improvements (#163)
    * Improved pagination and added ability for recuring tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,16 +1,25 @@
 """Admin configuration for the Tasks application."""
 from django.contrib import admin
 
-from tasks.models import Task
+from tasks.models import Task, TaskFrequency
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     """Admin for the Task model."""
 
-    list_display = ("title", "description", "completed", "created_at", "due_by")
+    list_display = ("title", "completed", "created_at", "due_by")
     list_filter = ("completed",)
-    search_fields = ("title", "description")
+    search_fields = ("title",)
     ordering = ("due_by",)
     date_hierarchy = "due_by"
     list_per_page = 20
+
+
+@admin.register(TaskFrequency)
+class TaskFrequencyAdmin(admin.ModelAdmin):
+    """Admin for the TaskFrequency Admin."""
+
+    list_display = ("title",)
+    search_fields = ("title",)
+    ordering = ("title",)
