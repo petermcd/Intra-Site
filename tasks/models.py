f982572c811e86f47f8b7aa877025a6c43723a06
Peter McDonald <me@petermcdonald>
2022-08-05 00:20:11 +0100

    Improved pagination and added ability for recuring tasks

--- tasks/models.py
+++ tasks/models.py
@@ -5,15 +5,40 @@ from django.db import models
 class Task(models.Model):
     """Model for Tasks."""
 
-    title: models.CharField = models.CharField(max_length=200)
-    description: models.TextField = models.TextField()
+    title: models.CharField = models.CharField(max_length=200, blank=False, null=False)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
     updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
     due_by: models.DateTimeField = models.DateTimeField()
     completed: models.BooleanField = models.BooleanField(default=False)
+    frequency: models.ForeignKey = models.ForeignKey(
+        to="TaskFrequency", on_delete=models.RESTRICT, blank=True, null=True
+    )
 
     class Meta:
         """Meta class."""
 
         verbose_name = "Task"
         verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        """Return the task name."""
+        return self.title
+
+
+class TaskFrequency(models.Model):
+    """Model for Tasks."""
+
+    title: models.CharField = models.CharField(max_length=200)
+    days_to_add: models.IntegerField = models.IntegerField(default=0)
+    months_to_add: models.IntegerField = models.IntegerField(default=0)
+    years_to_add: models.IntegerField = models.IntegerField(default=0)
+
+    class Meta:
+        """Meta class."""
+
+        verbose_name = "Task Frequency"
+        verbose_name_plural = "Task Frequencies"
+
+    def __str__(self):
+        """Return the task frequency name."""
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,12 @@
 """Views for the Task application."""
 from datetime import datetime
 
+from dateutil.relativedelta import relativedelta
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
-from tasks.models import Task
+from tasks.models import Task, TaskFrequency
 
 
 class TaskView(generic.ListView):
@@ -36,9 +37,15 @@ def task_add(request):
         Rendered form
     """
     task_item = Task()
-    task_item.description = request.POST["task-text"]
+    task_item.title = request.POST["task-title"]
     due_by = datetime.strptime(request.POST["task-due-by"], "%d-%m-%Y %H:%M")
     task_item.due_by = due_by
+    if len(request.POST["task-frequency"]) == 1:
+        task_frequency = TaskFrequency.objects.filter(
+            pk=request.POST["task-frequency"][0]
+        )
+        if len(task_frequency) == 1:
+            task_item.frequency = task_frequency[0]
     task_item.save()
     context = {"task": task_item}
     return render(request, "tasks/partials/task_item.html", context)
@@ -57,7 +64,20 @@ def task_complete(request, pk: int):
     """
     task_item = Task.objects.filter(pk=pk)
     if len(task_item) == 1:
-        task_item[0].completed = True
+        task_frequency = task_item[0].frequency
+        if not task_frequency or (
+            task_frequency.days_to_add == 0
+            and task_frequency.months_to_add == 0
+            and task_frequency.years_to_add == 0
+        ):
+            task_item[0].completed = True
+        else:
+            dt = relativedelta(
+                days=task_frequency.days_to_add,
+                months=task_frequency.months_to_add,
+                years=task_frequency.years_to_add,
+            )
+            task_item[0].due_by += dt
         task_item[0].save()
     return HttpResponse(status=200)
 
@@ -72,5 +92,6 @@ def task_output_form(request):
     Returns:
         Rendered form
     """
-    context = {}
+    context = {"task_frequencies": list(TaskFrequency.objects.all())}
+    print(context)
     return render(request, "tasks/partials/task_add_form.html", context)
