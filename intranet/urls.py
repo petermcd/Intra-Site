cb426e02fda10e56af44dec27f19a257acc182bf
Peter McDonald <me@petermcdonald.co.uk>
2022-10-30 21:15:47 +0000

    changed folder name for downloads

--- intranet/urls.py
+++ intranet/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("books/", include("books.urls")),
     path("documents/", include("documents.urls")),
-    path("downloads/", include("downloads.urls")),
+    path("SiteDocuments/", include("downloads.urls")),
     path("events/", include("events.urls")),
     path("favicon.ico", RedirectView.as_view(url=static("img/favicon.ico"))),
     path("finance/", include("finance.urls")),
