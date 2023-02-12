016c96b367fcc5d83c9861de1639792bf325ecec
Peter McDonald <148006+petermcd@users.noreply.github.com>
2023-02-12 14:44:27 +0000

    Sanitized user input

--- downloads/views.py
+++ downloads/views.py
@@ -1,10 +1,14 @@
 """Views for Downloads."""
+import re
 from os.path import join, normpath
+from typing import Union
 
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponseNotFound
 
 
-def download_view(request, directory="", filename="") -> FileResponse:
+def download_view(
+    request, directory="", filename=""
+) -> Union[FileResponse, HttpResponseNotFound]:
     """
     Facilitate downloads.
 
@@ -14,7 +18,12 @@ def download_view(request, directory="", filename="") -> FileResponse:
         filename: File name of the file to download
 
     Returns:
-        FileResponse object with content of the file
+        FileResponse object with content of the file or HttpResponseNotFound on failure
     """
-    file_path = normpath(join("SiteDocuments", directory, filename))
-    return FileResponse(open(file_path, "rb"))
+    if re.match(r"^[a-zA-Z0-9_-]+$", directory) and re.match(
+        r"^[a-zA-Z0-9_-]+\.[a-zA-Z0-9]+$", filename
+    ):
+        file_path = normpath(join("SiteDocuments", directory, filename))
+        return FileResponse(open(file_path, "rb"))
+
+    return HttpResponseNotFound()
